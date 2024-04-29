574b0fe6fceec7550dc627a9ecfd407c7e0299f3
Lukas Skl <lucasdure380@gmail.com>
2024-04-28 21:49:26 -0300

    Sprint 1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener  } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { Auth, signOut } from '@angular/fire/auth';
 import { LoginComponent } from './components/login/login.component';
+import { Sidebar } from 'primeng/sidebar';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, ButtonModule, MenubarModule, LoginComponent],
+  imports: [RouterOutlet, RouterModule, ButtonModule, MenubarModule, LoginComponent, SidebarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,6 +21,7 @@ export class AppComponent implements OnInit {
   sidebarVisible: boolean = false;
   visible: boolean = false;
   sesion: boolean = false;
+  isSmallScreen: boolean = false;
 
   constructor(private router: Router, public auth: Auth){
 
@@ -32,6 +35,7 @@ export class AppComponent implements OnInit {
   items: MenuItem[] | undefined;
 
   ngOnInit() {
+    this.checkScreenSize();
     this.auth.onAuthStateChanged(user => {
       if (user) {
         this.sesion = true;
@@ -55,5 +59,21 @@ export class AppComponent implements OnInit {
       console.log(this.auth.currentUser?.email);
     })
   }
-  
+
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+  closeCallback(e:any): void {
+      this.sidebarRef.close(e);
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:Event) {
+    // Llama a la función checkScreenSize cada vez que el tamaño de la ventana cambia
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isSmallScreen = window.innerWidth < 768; // Cambia 768 por el tamaño de pantalla que desees
+  }
+
 }
